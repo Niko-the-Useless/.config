9283b98262b49240c1961486899be25f3473adad
Niko <radosz2003@gazeta.pl>
2024-05-08 22:01:40 +0200

    .

---
+++ nvim/lua/core/plugins_config/lsp_config.lua
@@ -0,0 +1,26 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {"lua_ls"}
+})
+
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("lspconfig").lua_ls.setup {
+capabilities = capabilities,
+}
+require("lspconfig").rust_analyzer.setup {
+capabilities = capabilities,
+}
+require("lspconfig").clangd.setup {
+capabilities = capabilities,
+}
---
+++ nvim/lua/core/visuals.lua
@@ -0,0 +1,4 @@
+vim.opt.syntax = on
+vim.opt.number = true
+vim.o.termguicolors = true
+vim.cmd[[colorscheme dracula]]
